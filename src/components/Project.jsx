af39b9ac8684c87c4da64eae3608cdb8e5d16a0f
miawithcode <cuttontail@gmail.com>
2024-02-04 17:34:15 +0800

    feat: render project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,40 @@ import { useFetchProject } from '../fetchProject';
 
 const Project = () => {
   const { loading, projects } = useFetchProject();
-  console.log(loading, projects);
-  return <div>Project</div>;
+
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>Projects</h2>
+        <div className="title-underline"></div>
+      </div>
+
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { title, url, img, id } = project;
+          return (
+            <a
+              href={url}
+              key={id}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 export default Project;
